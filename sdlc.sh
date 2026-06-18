a74972f669f8e919e0ef70aae1f7988aba9dc725
Dr. Jeffrey Chijioke-Uche <sj@chijioke-uche.com>
2026-06-18 17:06:21 -0400

    Update Qiskit installer Wizard Application

--- sdlc.sh
+++ sdlc.sh
@@ -1,4 +1,19 @@
 
+#!/usr/bin/env bash
+
+#---------------------------------------------------------------------------------------------
+# @Author: Dr. Jeffrey Chijioke-Uche
+# @Project: Quantum Computing Qiskit v2.x Tool Wizard
+# @Technology: Quantum Computing, Qiskit
+# @File: qiskit-v2x-install-wizard.sh
+# @Description: Qiskit v2.x Installer Wizard
+# @Version: 1.0.4-production-seeded-pip-verified
+# @Date: 2024-01-01
+# @License: Apache-2.0
+# @Title: Qiskit v2.x Installer Wizard
+# @Software Author: Dr. Jeffrey Chijioke-Uche, IBM Computer Science & Quantum Ambassador
+#---------------------------------------------------------------------------------------------
 git add -A
-git commit -m "Update Qiskit installer wizard scripts"
+git commit -m "Update Qiskit installer Wizard Application"
 git push
+chmod 777 *.sh
